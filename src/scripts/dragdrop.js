2e355cf77cbdc0fa77c0b5a625c04cdc68cfa019
Jennifer Lawson <jenniferlawson000@gmail.com>
2018-06-20 10:51:16 -0500

    fixed drag drop sort of

--- src/scripts/dragdrop.js
+++ src/scripts/dragdrop.js
@@ -1,19 +1,42 @@
-const allowDrop = (event) => {
-    event.preventDefault();
- }
+const database = require("./database")
+const saveDatabase = require("./saveLocal")
 
-const drag = (event) => {
-    event.dataTransfer.setData("text", event.target.id);
- }
+const drag = (event) => { //ondragstart attached to task
+    console.log("drag started");
+    event.dataTransfer.setData("text", event.target.classList);
+    console.log(event.target.classList);
 
-const drop = () => {
+
+}
+
+const allowDrop = (event) => { //attached to column
+    console.log("ondragover");
     event.preventDefault();
-    var data = event.dataTransfer.getData("text");
-    // if (el.id === "toDo") {
-    //     alert("Cant do that")
-    // }  else
-    this.appendChild(document.getElementById(data));
- }
+
+}
+
+const drop = function(event) { //attached to column
+    event.preventDefault();
+    console.log("ondrop");
+
+    let data = event.dataTransfer.getData("text");
+    console.log(event.target, data);
+
+    event.target.appendChild(document.querySelector(`.${data}`))
+
+    database[event.target] = event;
+    console.log(event.target)
+    saveDatabase(database, "database")
+}
+
+// let targets = document.querySelector(".column");
+// targets.forEach(target => {
+//     // if (target.id === "toDo") {
+//         //     alert("Cant do that")
+//         // }  else
+//         target.ondragover = allowDrop;
+//         target.ondrop = drop;
+//     })
 
 
  module.exports = {allowDrop, drag, drop}
