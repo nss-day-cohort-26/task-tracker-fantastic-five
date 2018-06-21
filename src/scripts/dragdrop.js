1ea95de898ddc0afa5db5e1209da816e9dff8d29
Jennifer Lawson <jenniferlawson000@gmail.com>
2018-06-21 09:15:58 -0500

    fixed local storage

--- src/scripts/dragdrop.js
+++ src/scripts/dragdrop.js
@@ -24,6 +24,14 @@ const drop = function(event) { //attached to column
 
     event.target.appendChild(document.querySelector(`.${data}`))
 
+    if ( event.target.id === "done") {
+        let archiveBtn = document.createElement("button")
+        archiveBtn.type = "button";
+        archiveBtn.addEventListener("click", archiveMode)
+        archiveBtn.textContent = "Archive"
+        event.target.lastChild.appendChild(archiveBtn)
+    }
+
     console.log("x: ", event.target.lastChild)
 
     var myId = event.target.lastChild.id.toString();
@@ -50,11 +58,7 @@ const drop = function(event) { //attached to column
             myObj = database.done[item];
             delete database.done[item]
             let targetDiv = event.target.lastChild
-            let archiveBtn = document.createElement("button")
-            archiveBtn.type = "button";
-            archiveBtn.addEventListener("click", archiveMode)
-            archiveBtn.textContent = "Archive"
-            targetDiv.appendChild(archiveBtn)
+
         }
     }
 
